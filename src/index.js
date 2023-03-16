79658195a4208fa5380d40f2fb6c62c7bb492b87
dieum512 <akonkwadieum222@gmail.com>
2023-03-16 18:43:50 +0200

    Add the JS functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 import './style.css';
+const name = document.querySelector('.add-name-input');
+const score = document.querySelector('.add-sccore-input');
+const ul = document.querySelector('.scores-ul');
+const refresh = document.querySelector('.refresh-btn');
+const form = document.querySelector('.add-form')
+
+const SendData = (name, score) => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/', {
+    method: 'POST',
+    body: JSON.stringify({ score, user: name }),
+    headers: {
+      'content-type': 'application/json'
+    },
+  });
+};
+
+const displayData = (storedData) => {
+  ul.innerHTML = '';
+  storedData.forEach((element) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <span class="score-user-name">${element.user}:</span>
+      <span class="score-user-score">${element.score}</span>
+    `
+
+    ul.appendChild(li);
+  })
+}
+
+const getData = async () => {
+  const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/');
+  const data = await res.json();
+  const { result } = data;
+  displayData(result);
+}
+
+form.addEventListener('submit', () => {
+  SendData(name.value, score.value)
+})
+
+refresh.addEventListener('click', () => {
+  getData();
+  window.location.reload();
+})
+
+window.addEventListener('load', () => {
+  getData();
+});
